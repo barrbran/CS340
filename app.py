09c34446c39df6c33a268153b2a29fb7d255546a
barrbran <54120086+barrbran@users.noreply.github.com>
2021-03-04 23:39:42 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -351,6 +351,14 @@ def delete_manager(id):
     print(str(result.rowcount) + " row(s) updated")
     return redirect('/browse_manager')
 
+@app.errorhandler(500)
+def internal_error(error):
+    return "500 error"
+
+@app.errorhandler(404)
+def not_found(error):
+    return "404 error",404
+
 # Listener
 
 if __name__ == "__main__":
