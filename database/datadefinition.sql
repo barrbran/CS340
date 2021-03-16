afaa32820eefb03c68dbc739bf852b4cfb10e40c
SawyerPaeth <59540217+SawyerPaeth@users.noreply.github.com>
2021-03-15 20:14:21 -0700

    Update datadefinition.sql

--- database/datadefinition.sql
+++ database/datadefinition.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 05, 2021 at 04:36 AM
+-- Generation Time: Feb 15, 2021 at 04:00 AM
 -- Server version: 10.4.17-MariaDB-log
 -- PHP Version: 7.4.13
 
@@ -33,7 +33,7 @@ CREATE TABLE `doctor` (
   `lname` varchar(255) NOT NULL,
   `phoneNumber` varchar(255) NOT NULL,
   `salary` int(11) NOT NULL,
-  `nurseID` int(11) DEFAULT NULL
+  `nurseID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -41,10 +41,8 @@ CREATE TABLE `doctor` (
 --
 
 INSERT INTO `doctor` (`id`, `fname`, `lname`, `phoneNumber`, `salary`, `nurseID`) VALUES
-(1, 'Susan', 'Zoom', '345-099-0493', 150000, 10),
-(2, 'Troy', 'Albertson', '345-543-5544', 145000, 9),
-(3, 'Buddy', 'Baker', '6723449875', 98765, 9),
-(4, 'Jill', 'Biden', '555-555-5555', 5000000, 10);
+(1, 'Susan', 'Zoom', '345-099-0493', 150000, 8),
+(2, 'Troy', 'Albertson', '345-543-5544', 145000, 9);
 
 -- --------------------------------------------------------
 
@@ -65,9 +63,7 @@ CREATE TABLE `manager` (
 --
 
 INSERT INTO `manager` (`id`, `fname`, `lname`, `phoneNumber`, `salary`) VALUES
-(1, 'Judy', 'Boris', '444-567-9483', 90008),
-(2, 'Ron', 'West', '433-343-3532', 50000),
-(3, 'Sally', 'Rio', '1234565555', 40000);
+(1, 'Judy', 'Boris', '444-567-9483', 90000);
 
 -- --------------------------------------------------------
 
@@ -88,8 +84,8 @@ CREATE TABLE `nurse` (
 --
 
 INSERT INTO `nurse` (`id`, `fname`, `lname`, `phoneNumber`, `salary`) VALUES
-(9, 'Gina', 'Peters', '333-454-3455', 55255),
-(10, 'Phil', 'Melancon', '9876543210', 65437);
+(8, 'Alice', 'Johnson', '333-454-3422', 55000),
+(9, 'Gina', 'Peters', '333-454-3455', 55255);
 
 -- --------------------------------------------------------
 
@@ -105,7 +101,7 @@ CREATE TABLE `office` (
   `city` varchar(255) NOT NULL,
   `state` varchar(255) NOT NULL,
   `zip` int(11) NOT NULL,
-  `managerID` int(11) DEFAULT NULL
+  `managerID` int(11)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -113,13 +109,9 @@ CREATE TABLE `office` (
 --
 
 INSERT INTO `office` (`id`, `name`, `phoneNumber`, `street`, `city`, `state`, `zip`, `managerID`) VALUES
-(1, 'Main Clinic', '445-453-3422', '123 Main Street', 'Corvallis', 'OR', 97330, 1),
-(2, 'The Biltmore', '343-444-2345', '444 Tree Drive', 'Corvallis', 'OR', 97331, 1),
-(4, 'The Sick Place', '345-324-3452', '444 Nowhere Ave', 'Corvallis', 'OR', 97331, 1),
-(5, 'Test Office', '123-456-5555', '34 1st St.', 'Seattle', 'WA', 53212, 2);
+(1, 'Main Clinic', '445-453-3422', '123 Main Street', 'Corvallis', 'OR', 97330, 1);
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `officedoctor`
 --
@@ -137,7 +129,6 @@ CREATE TABLE `officedoctor` (
 INSERT INTO `officedoctor` (`id`, `officeID`, `doctorID`) VALUES
 (1, 1, 1);
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `patient`
@@ -154,7 +145,7 @@ CREATE TABLE `patient` (
   `zip` int(11) NOT NULL,
   `dob` date NOT NULL,
   `weight` int(11) NOT NULL,
-  `doctorID` int(11) DEFAULT NULL
+  `doctorID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -162,10 +153,7 @@ CREATE TABLE `patient` (
 --
 
 INSERT INTO `patient` (`id`, `fname`, `lname`, `phoneNumber`, `street`, `city`, `state`, `zip`, `dob`, `weight`, `doctorID`) VALUES
-(1, 'Harris', 'Harrison', '455-455-4555', '78 Pine Avenue', 'Corvallis', 'OR', 97330, '2000-12-08', 140, 1),
-(2, 'Horace', 'Buxley', '455-567-4433', '899 Illness Rd', 'Corvallis', 'OR', 97330, '2001-04-14', 344, 2),
-(3, 'Horace', 'Buxley', '455-567-4433', '899 Illness Rd', 'Corvallis', 'OR', 97330, '2001-04-14', 344, 2),
-(4, 'Bill', 'Williams', '5555555555', 'house st', 'seattle', 'washington', 99999, '1980-09-04', 170, 1);
+(1, 'Harris', 'Harrison', '455-455-4555', '78 Pine Avenue', 'Corvallis', 'OR', 97330, '2000-12-08', 150, 1);
 
 --
 -- Indexes for dumped tables
@@ -177,6 +165,14 @@ INSERT INTO `patient` (`id`, `fname`, `lname`, `phoneNumber`, `street`, `city`, 
 ALTER TABLE `doctor`
   ADD PRIMARY KEY (`id`),
   ADD KEY `nurseID` (`nurseID`);
+
+--
+-- Indexes for table `officedoctor`
+--
+ALTER TABLE `officedoctor`
+  ADD PRIMARY KEY (`id`,`officeID`,`doctorID`),
+  ADD KEY `assignment_fk_1` (`doctorID`);
+
 
 --
 -- Indexes for table `manager`
@@ -198,17 +194,6 @@ ALTER TABLE `office`
   ADD KEY `office_fk_1` (`managerID`);
 
 --
--- Indexes for table `officedoctor`
---
---ALTER TABLE `officedoctor`
---  ADD PRIMARY KEY (`officeID`,`doctorID`),
---  ADD KEY `assignment_fk_1` (`doctorID`);
-  
- ALTER TABLE `officedoctor`
-  ADD PRIMARY KEY (`id`,`officeID`,`doctorID`),
-  ADD KEY `assignment_fk_1` (`doctorID`);
-
---
 -- Indexes for table `patient`
 --
 ALTER TABLE `patient`
@@ -223,35 +208,37 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-  
- -- AUTO_INCREMENT for table `officedoctor` 
- ALTER TABLE `officedoctor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `officedoctor`
+--
+ALTER TABLE `officedoctor`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `manager`
 --
 ALTER TABLE `manager`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `nurse`
 --
 ALTER TABLE `nurse`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `office`
 --
 ALTER TABLE `office`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -261,15 +248,14 @@ ALTER TABLE `patient`
 -- Constraints for table `doctor`
 --
 ALTER TABLE `doctor`
-  ADD CONSTRAINT `doctor_fk_1` FOREIGN KEY (`nurseID`) REFERENCES `nurse` (`id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `doctor_fk_1` FOREIGN KEY (`nurseID`) REFERENCES `nurse` (`id`);
 
 --
 -- Constraints for table `office`
 --
 ALTER TABLE `office`
-  ADD CONSTRAINT `office_fk_1` FOREIGN KEY (`managerID`) REFERENCES `manager` (`id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `office_fk_1` FOREIGN KEY (`managerID`) REFERENCES `manager` (`id`);
 
---
 -- Constraints for table `officedoctor`
 --
 ALTER TABLE `officedoctor`
@@ -280,9 +266,10 @@ ALTER TABLE `officedoctor`
 -- Constraints for table `patient`
 --
 ALTER TABLE `patient`
-  ADD CONSTRAINT `patient_fk_1` FOREIGN KEY (`doctorID`) REFERENCES `doctor` (`id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `patient_fk_1` FOREIGN KEY (`doctorID`) REFERENCES `doctor` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
